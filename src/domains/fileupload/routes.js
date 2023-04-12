76ecd70033bbaa0cab571908d429a0f56e670a94
Rima Achour <rimaachour57@gmail.com>
2023-04-12 23:44:16 +0100

    last update

--- src/domains/fileupload/routes.js
+++ src/domains/fileupload/routes.js
@@ -2,7 +2,32 @@
 const multer = require('multer');
 const express = require('express');
 const router = express.Router();
-const upload = multer({ dest: 'uploads/' }); // specify the directory to store uploaded files
+const mime = require('mime-types');
+
+const imageFileFilter = (req, file, callback) => {
+  // Get the MIME type of the uploaded file
+  const mimeType = mime.lookup(file.originalname);
+
+  // Check if the MIME type is an image type
+  if (mimeType.startsWith('image/')) {
+    callback(null, true);
+  } else {
+    callback(new Error('Invalid file type. Only image files are allowed.'));
+  }
+};
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+      callback(null, 'uploads/');
+    },
+    filename: (req, file, callback) => {
+      const fileName = file.originalname.toLowerCase().split(' ').join('-');
+      callback(null, fileName);
+    },
+  });
+  const upload = multer({
+    storage: storage,
+    fileFilter: imageFileFilter,
+  });
 const fileuploadController = require('./controller')
 router.post('/uploadfile',upload.single('file'),fileuploadController.uploadHandler)
 router.get('/image/:filename',fileuploadController.getfile)
