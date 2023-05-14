7a140fd32790cdc408b250948a60c244d63e001b
Ahmed Sbai <ahmedclubust@gmail.com>
2023-05-14 11:05:18 +0200

    Fix: Fixing Relations wont be created when migrating table pref

--- src/domains/preference/model.js
+++ src/domains/preference/model.js
@@ -1,7 +1,12 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = require('../../config/db');
-const Student = require('../student/model')
-const preference = sequelize.define('pref', {
+const {Domain} = require("../domain/model");
+const Skill = require("../skills/model");
+const Student = require("../student/model");
+
+console.log('---------man', Domain, Student)
+
+const Preference = sequelize.define('pref', {
     id: {
       type: Sequelize.INTEGER,
       primaryKey: true,
@@ -11,12 +16,35 @@ const preference = sequelize.define('pref', {
       type: Sequelize.STRING,
       allowNull: false
     },
-   
+    domainID: {
+        type: Sequelize.INTEGER,
+        references: {
+            model: Domain,
+            key: 'id'
+        }
+    },
+    studentID: {
+        type: Sequelize.INTEGER,
+        references: {
+            model: 'students', // Wrote it hardcoded due to circular dependencies causing it to be empty object
+            //TODO fix circular deps
+            key: 'id'
+        }
+    }
   }, {
     //tableName: 'pref',
     timestamps: false
   });
-  
+
+Preference.associate = (models) => {
+    Preference.hasMany(Domain);
+    Preference.hasMany(Student);
+    Domain.belongsTo(Preference)
+    Student.belongsTo(Preference)
+
+};
+// preference.belongsTo(User, {as: 'User', foreignKey: 'UserID'});
+
 sequelize.sync({ force: false }).then(() => {
   console.log('Tables created successfully');
 }).catch((err) => {
@@ -24,4 +52,5 @@ sequelize.sync({ force: false }).then(() => {
 });
 
 
-  module.exports = preference;
+console.log('---------man', Preference)
+module.exports = Preference;
